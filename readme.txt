8069acffa46ae047cd87986233d70117c3ed6b27
wp-chandra <wp-chandra@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-03 04:54:50 +0000

    Using WP Live Comments Validation plugin to validate comments form using jquery validation. This plugin allow you to easy way to validate comment form.
    git-svn-id: https://plugins.svn.wordpress.org/wp-live-comments-validation/trunk@1126760 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,25 @@
+=== WP Live Comments Validation  ===
+Contributors: wp-chandra
+Donate link:https://www.wpchandra.com/
+Tags: comments, comments validation, wpchandra, live validation,jquery validation
+Requires at least: 3.0.1
+Tested up to: 3.4
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Using WP Live Comments Validation plugin to validate comments form using jquery validation. This plugin allow you to easy way to validate comment form.
+
+== Description ==
+
+Using WP Live Comments Validation plugin to validate comments form using jquery validation. This plugin allow you to easy way to validate comment form.
+
+== Installation ==
+
+Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your WordPress installation and then activate the Plugin from Plugins page.
+
+== Screenshots ==
+1. See WP Live Comments Validation!
+
+== Changelog ==
+v1.0
---
+++ wp-live-comments-validation.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ Plugin Name: WP Live Comments Validation
+ Plugin URI: http://www.wpchandra.com/
+ Description: Using WP Live Comments Validation plugin to validate comments form using jquery validation. This plugin allow you to easy way to validate comment form.
+ Version: 1.0
+ Author: Chandrakesh Kumar
+ Author URI: http://www.wpchandra.com/
+ License: GPL3
+ */
+ 
+ //add jquery validation and jquery 
+ function wp_live_comments_validation_scripts() {
+	if(is_single() ) {
+		wp_enqueue_script('jquery-validate',plugin_dir_url( __FILE__ ) . 'js/jquery.validate.min.js',array('jquery'),'1.10.0',true);
+		wp_enqueue_style('jquery-validate',plugin_dir_url( __FILE__ ) . 'styles/style.css',array(),'1.0');
+	}
+}
+function wp_live_comments_validation_init() { ?>
+	<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			
+			$('#commentform').validate({
+				rules: {
+					author: {
+						required: true,
+						minlength: 2
+					},
+					
+					email: {
+						required: true,
+						email: true
+					},
+					
+					url: {
+						url: true
+					},
+					
+					comment: {
+						required: true,
+						minlength: 20
+					}
+				},
+				
+				messages: {
+					author: "Please enter a valid name.",
+					email: "Please enter a valid email address.",
+					url: "Please a valid website address.",
+					comment: "Message must be at least 20 characters."
+				}
+			});
+		});
+	</script>
+<?php }
+//add jquery validation
+add_action('template_redirect', 'wp_live_comments_validation_scripts');
+//add scripts to footer
+add_action('wp_footer', 'wp_live_comments_validation_init', 999);
+
+?>
